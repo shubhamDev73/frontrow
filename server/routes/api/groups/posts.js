f0057442b6e3bfcd3e5c2412c1fa04bca62e32d7
Shubham Gupta <shubham0209@gmail.com>
2020-07-07 14:52:33 +0530

    Added group posts by member type option

--- server/routes/api/groups/posts.js
+++ server/routes/api/groups/posts.js
@@ -71,40 +71,32 @@ router.get('/list/', (req, res) => {
 });
 
 router.get('/by/', (req, res) => {
-	const dates = essentials.getDates(req);
 	const connection = new Connection();
 	connection.connect(() => {
 		connection.queries = 1;
 		if(req.query.type){
-			connection.execute("SELECT `user`, COUNT(*) as `total` FROM `post` WHERE `group` = ? AND `time` BETWEEN ? AND ? AND `type` = ? GROUP BY `user` ORDER BY COUNT(*) DESC;", [group.id, dates[0], dates[1], req.query.type], res, (results) => {
+			connection.execute("SELECT `user`, COUNT(*) as `total` FROM `post` WHERE `group` = ? AND `type` = ? GROUP BY `user` ORDER BY COUNT(*) DESC;", [group.id, req.query.type], res, (results) => {
 				connection.response = essentials.count(results, 'user');
 			});
 		}else{
-			connection.execute("SELECT `user`, COUNT(*) as `total` FROM `post` WHERE `group` = ? AND `time` BETWEEN ? AND ? GROUP BY `user` ORDER BY COUNT(*) DESC;", [group.id, dates[0], dates[1]], res, (results) => {
+			connection.execute("SELECT `user`, COUNT(*) as `total` FROM `post` WHERE `group` = ? GROUP BY `user` ORDER BY COUNT(*) DESC;", [group.id], res, (results) => {
 				connection.response = essentials.count(results, 'user');
 			});
 		}
 	});
 });
 
-router.get('/by/:user/', (req, res) => {
-	const dates = essentials.getDates(req);
+router.get('/by/:user(\\d+)/', (req, res) => {
 	const connection = new Connection();
 	connection.connect(() => {
 		connection.queries = 1;
-		if(req.query.type){
-			connection.execute("SELECT COUNT(*) as `total` FROM `post` WHERE `group` = ? AND `user` = ? AND `time` BETWEEN ? AND ? AND `type` = ?;", [group.id, req.params.user, dates[0], dates[1], req.query.type], res, (results) => {
-				connection.response = results[0];
-			});
-		}else{
-			connection.execute("SELECT COUNT(*) as `total` FROM `post` WHERE `group` = ? AND `user` = ? AND `time` BETWEEN ? AND ?;", [group.id, req.params.user, dates[0], dates[1]], res, (results) => {
-				connection.response = results[0];
-			});
-		}
+		connection.execute("SELECT `type`, COUNT(*) as `total` FROM `post` WHERE `group` = ? AND `user` = ? GROUP BY `type`;", [group.id, req.params.user], res, (results) => {
+			connection.response = essentials.count(results, 'type', ['poll', 'first', 'link', 'share', 'with']);
+		});
 	});
 });
 
-router.get('/by/:user/new/', (req, res) => {
+router.get('/by/:user(\\d+)/new/', (req, res) => {
 	const dates = essentials.getDates(req);
 	const connection = new Connection();
 	connection.connect(() => {
@@ -121,7 +113,7 @@ router.get('/by/:user/new/', (req, res) => {
 	});
 });
 
-router.get('/by/:user/total/', (req, res) => {
+router.get('/by/:user(\\d+)/total/', (req, res) => {
 	const dates = essentials.getDates(req);
 	const connection = new Connection();
 	connection.connect(() => {
@@ -138,7 +130,7 @@ router.get('/by/:user/total/', (req, res) => {
 	});
 });
 
-router.get('/by/:user/list/', (req, res) => {
+router.get('/by/:user(\\d+)/list/', (req, res) => {
 	const dates = essentials.getDates(req);
 	const connection = new Connection();
 	connection.connect(() => {
@@ -149,6 +141,67 @@ router.get('/by/:user/list/', (req, res) => {
 			});
 		}else{
 			connection.execute("SELECT * FROM `post` WHERE `group` = ? AND `user` = ? AND `time` BETWEEN ? AND ? ORDER BY `time`;", [group.id, req.params.user, dates[0], dates[1]], res, (results) => {
+				connection.response = results;
+			});
+		}
+	});
+});
+
+router.get('/by/:type(\\w+)/', (req, res) => {
+	const connection = new Connection();
+	connection.connect(() => {
+		connection.queries = 1;
+		connection.execute("SELECT p.`type`, COUNT(*) as `total` FROM `post` p, `member` m WHERE p.`user` = m.`user` AND p.`group` = m.`group` AND p.`group` = ? AND m.`leave_time` IS NULL AND m.`member_type` = ? GROUP BY p.`type`;", [group.id, req.params.type], res, (results) => {
+			connection.response = essentials.count(results, 'type', ['poll', 'first', 'link', 'share', 'with']);
+		});
+	});
+});
+
+router.get('/by/:type(\\w+)/new/', (req, res) => {
+	const dates = essentials.getDates(req);
+	const connection = new Connection();
+	connection.connect(() => {
+		connection.queries = 1;
+		if(req.query.type){
+			connection.execute("SELECT `time` FROM `post` p, `member` m WHERE p.`user` = m.`user` AND p.`group` = m.`group` AND p.`group` = ? AND m.`leave_time` IS NULL AND m.`member_type` = ? AND `time` BETWEEN ? AND ? AND p.`type` = ? ORDER BY `time`;", [group.id, req.params.type, dates[0], dates[1], req.query.type], res, (results) => {
+				connection.response = essentials.periodify(results, 'time', dates, false, req.query.period);
+			});
+		}else{
+			connection.execute("SELECT `time` FROM `post` p, `member` m WHERE p.`user` = m.`user` AND p.`group` = m.`group` AND p.`group` = ? AND m.`leave_time` IS NULL AND m.`member_type` = ? AND `time` BETWEEN ? AND ? ORDER BY `time`;", [group.id, req.params.type, dates[0], dates[1]], res, (results) => {
+				connection.response = essentials.periodify(results, 'time', dates, false, req.query.period);
+			});
+		}
+	});
+});
+
+router.get('/by/:type(\\w+)/total/', (req, res) => {
+	const dates = essentials.getDates(req);
+	const connection = new Connection();
+	connection.connect(() => {
+		connection.queries = 1;
+		if(req.query.type){
+			connection.execute("SELECT `time` FROM `post` p, `member` m WHERE p.`user` = m.`user` AND p.`group` = m.`group` AND p.`group` = ? AND m.`leave_time` IS NULL AND m.`member_type` = ? AND `time` BETWEEN ? AND ? AND p.`type` = ? ORDER BY `time`;", [group.id, req.params.type, dates[0], dates[1], req.query.type], res, (results) => {
+				connection.response = essentials.periodify(results, 'time', dates, true, req.query.period);
+			});
+		}else{
+			connection.execute("SELECT `time` FROM `post` p, `member` m WHERE p.`user` = m.`user` AND p.`group` = m.`group` AND p.`group` = ? AND m.`leave_time` IS NULL AND m.`member_type` = ? AND `time` BETWEEN ? AND ? ORDER BY `time`;", [group.id, req.params.type, dates[0], dates[1]], res, (results) => {
+				connection.response = essentials.periodify(results, 'time', dates, true, req.query.period);
+			});
+		}
+	});
+});
+
+router.get('/by/:type(\\w+)/list/', (req, res) => {
+	const dates = essentials.getDates(req);
+	const connection = new Connection();
+	connection.connect(() => {
+		connection.queries = 1;
+		if(req.query.type){
+			connection.execute("SELECT * FROM `post` p, `member` m WHERE p.`user` = m.`user` AND p.`group` = m.`group` AND p.`group` = ? AND m.`leave_time` IS NULL AND m.`member_type` = ? AND `time` BETWEEN ? AND ? AND p.`type` = ? ORDER BY `time`;", [group.id, req.params.type, dates[0], dates[1], req.query.type], res, (results) => {
+				connection.response = results;
+			});
+		}else{
+			connection.execute("SELECT * FROM `post` p, `member` m WHERE p.`user` = m.`user` AND p.`group` = m.`group` AND p.`group` = ? AND m.`leave_time` IS NULL AND m.`member_type` = ? AND `time` BETWEEN ? AND ? ORDER BY `time`;", [group.id, req.params.type, dates[0], dates[1]], res, (results) => {
 				connection.response = results;
 			});
 		}
